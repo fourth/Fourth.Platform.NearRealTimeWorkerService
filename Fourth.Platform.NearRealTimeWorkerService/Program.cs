cc9f314785fa59fbd5e5324a27dad5912566dd69
Lachezara Kostadinova <Lachezara.Kostadinova@fourth.com>
2022-05-26 17:41:29 +0300

    Add project files.

---
+++ Fourth.Platform.NearRealTimeWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using Fourth.Platform.NearRealTimeWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
